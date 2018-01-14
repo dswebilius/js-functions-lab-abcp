5b03854de2dcb835b080efaa5f84245dfc4bc331
Dave Swebilius <dswebilius@gmail.com>
2018-01-14 14:53:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@
 function happyHolidays(string) {
   return "Happy holidays!"
 }
-function happyHolidayTo (holiday, name) {
-    return `Happy ${holiday},${name}!`
+function happyHolidayTo (name) {
+    return `Happy holidays, ${name}!`
 }
